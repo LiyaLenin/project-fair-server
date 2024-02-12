02fa500dafa1e75fc8cced387f450d19b55d3910
Liya Lenin <liyaleninliya@gmail.com>
2024-02-13 02:19:52 +0530

    updated

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const router=express.Router()
 const userController=require('../Controllers/userController')
+const projectController=require('../Controllers/projectController')
+const jwtMiddleware = require('../Middlewares/jwtMiddleware')
+const multerConfig = require('../Middlewares/multerMiddleware')
+
+
 //route for register
 router.post('/register',userController.register)
 
 //login 
 router.post('/login',userController.login)
+//add project
+router.post('/addproject',jwtMiddleware,multerConfig.single('projectImage'), projectController.addProjects)
+//gethomeproject
+router.get('/home-projects',projectController.getHomeProjects)
+//getallprojects
+router.get('/all-projects',jwtMiddleware,projectController.getAllProjects)
 
-
-
+//get userprojects
+router.get('/user-projects',jwtMiddleware,projectController.getUserProjects)
+//edit project
+router.put('/project/edit/:pid',jwtMiddleware,multerConfig.single("projectImage"),projectController.editProject)
+//delete project
+router.delete('/project/remove/:pid',jwtMiddleware,projectController.removeProject)
+//updateuser
+router.put('/user/edit', jwtMiddleware, multerConfig.single('profileImage'), userController.editUser)
 module.exports=router
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ require('./DB/connection')
 pfServer.use(cors())
 pfServer.use(express.json())
 pfServer.use(router)
+pfServer.use('/uploads',express.static('./uploads'))
 
 const PORT=3000
 pfServer.listen(PORT,()=>{
